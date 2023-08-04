8e69d62b76cb15eb9454d292c4d035dc40b956de
Negami27 <yohei.negami@comsize.com>
2023-08-04 09:34:18 +0900

    Revert "add:登録システムのservlet,DAOの追加"
    This reverts commit 0e02f917814a92e1ee954551023c1a55a7752304.

--- src/servlet/TaskAddServlet.java
+++ src/servlet/TaskAddServlet.java
@@ -1,17 +1,12 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import model.entity.CategoryBean;
-import model.entity.StatusBean;
-import model.entity.UserBean;
 
 /**
  * Servlet implementation class TaskAddServlet
@@ -32,13 +27,8 @@ public class TaskAddServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		List<CategoryBean> categoryList =null;
-		List<UserBean> userNameList=null;
-		List<StatusBean> statusList=null;
-
-
-
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
