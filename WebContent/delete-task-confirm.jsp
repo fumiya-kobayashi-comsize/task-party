3fb997dd24eea1b493461f281a42be87dad622d1
KazusaKoseki <kazusa.koseki@comsize.com>
2023-08-04 14:32:08 +0900

    update:削除用のサーブレット,jspを更新

--- WebContent/delete-task-confirm.jsp
+++ WebContent/delete-task-confirm.jsp
@@ -8,5 +8,35 @@
 </head>
 <body>
 
+	<h2>タスク削除確認画面</h2>
+<hr>
+	<h3>次のタスクを削除します。</h3>
+<br>
+	<table>
+		<tr>
+			<th>タスク名</th>
+			<td></td>
+		</tr>
+		<tr>
+			<th>カテゴリ情報</th>
+			<td></td>
+		</tr>
+		<tr>
+			<th>期限</th>
+			<td></td>
+		</tr>
+		<tr>
+			<th>担当者情報</th>
+			<td></td>
+		</tr>
+		<tr>
+			<th>ステータス情報</th>
+			<td></td>
+		</tr>
+		<tr>
+			<th>メモ</th>
+			<td></td>
+		</tr>
+	</table>
 </body>
 </html>
--- src/servlet/TaskDeleteServlet.java
+++ src/servlet/TaskDeleteServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.dao.TaskDeleteDAO;
 
 /**
  * Servlet implementation class TaskDeleteServlet
@@ -13,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TaskDeleteServlet")
 public class TaskDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,8 +39,27 @@ public class TaskDeleteServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+
+		TaskDeleteDAO dao = new TaskDeleteDAO();
+		int processNumber = 0; //処理件数
+
+		try {
+			//処理
+			processNumber = dao.deleteTask(Integer.parseInt(request.getParameter("taskId")));
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		//処理件数で遷移先を
+		if(processNumber>0) {
+			//成功画面
+			RequestDispatcher rd = request.getRequestDispatcher("delete-task-success.jsp");
+			rd.forward(request, response);
+		} else {
+			//失敗画面
+			RequestDispatcher rd = request.getRequestDispatcher("delete-task-error.jsp");
+			rd.forward(request, response);
+		}
 	}
 
 }
