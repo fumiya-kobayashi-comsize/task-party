09cbefea616ae4b32de23104c726c3e351665061
Negami27 <yohei.negami@comsize.com>
2023-08-04 10:27:22 +0900

    add:登録画面でプルダウン表示するためのDAO,servletの追加

--- src/servlet/TaskAddServlet.java
+++ src/servlet/TaskAddServlet.java
@@ -1,12 +1,19 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.dao.TaskInsertDAO;
+import model.entity.CategoryBean;
+import model.entity.StatusBean;
+import model.entity.UserBean;
 
 /**
  * Servlet implementation class TaskAddServlet
@@ -15,26 +22,40 @@ import javax.servlet.http.HttpServletResponse;
 public class TaskAddServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TaskAddServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TaskAddServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		List<CategoryBean> categoryList = null;
+		List<UserBean> userList = null;
+		List<StatusBean> statusList = null;
+
+		TaskInsertDAO insertDAO = new TaskInsertDAO();
+
+		try {
+			categoryList = insertDAO.selectAllCategory();
+			userList = insertDAO.selectAllUser();
+			statusList = insertDAO.selectAllStatus();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 	}
 
