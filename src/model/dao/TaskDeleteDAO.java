59a7b67438a9647631c17c24508d16fb1375ed38
arakawa-comsize <ryukai.arakawa@comsize.com>
2023-08-04 10:28:36 +0900

    add : タスク削除用のDAO

--- src/model/dao/TaskDeleteDAO.java
+++ src/model/dao/TaskDeleteDAO.java
@@ -1,5 +1,29 @@
 package model.dao;
 
-public class TaskDeleteDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+/**
+ * タスクを削除するDAO
+ * @author arakawa
+ *
+ */
+public class TaskDeleteDAO {
+	/**
+	 * @param タスクID
+	 * @return 削除回数
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public int deleteTask(int taskId) throws ClassNotFoundException, SQLException {
+		int count = 0;
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement("DELETE FROM t_task WHERE task_id = ?")){
+			pstmt.setInt(1, taskId);
+			count = pstmt.executeUpdate();
+		}
+		return count;
+
+	}
 }
