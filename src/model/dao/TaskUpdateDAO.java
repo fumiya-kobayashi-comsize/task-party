6a8a9a7bf6d0ddd4d925381e01f0881c4e54c625
arakawa-comsize <ryukai.arakawa@comsize.com>
2023-08-04 11:57:24 +0900

    add : タスク更新DAOを実装

--- src/model/dao/TaskUpdateDAO.java
+++ src/model/dao/TaskUpdateDAO.java
@@ -1,5 +1,38 @@
 package model.dao;
 
-public class TaskUpdateDAO {
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import model.entity.TaskBean;
+
+/**
+ * タスク更新DAO
+ * @author arakawa
+ *
+ */
+public class TaskUpdateDAO {
+	/**
+	 * @param TaskBean
+	 * @return 更新回数
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public int updateItem(TaskBean task) throws ClassNotFoundException, SQLException {
+		int count = 0;
+		String sql = "UPDATE t_task SET task_name = ?, category_id = ?, limit_date = ?, user_id = ?, status_code = ?, memo = ?, WHERE task_id = ?";
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+			pstmt.setString(1, task.getTaskName());
+			pstmt.setInt(2, task.getCategoryId());
+			pstmt.setDate(3, Date.valueOf(task.getLimitDate()));
+			pstmt.setString(4, task.getUserId());
+			pstmt.setString(5, task.getStatusCode());
+			pstmt.setString(6, task.getMemo());
+			pstmt.setInt(7, task.getTaskId());
+			count = pstmt.executeUpdate();
+		}
+		return count;
+	}
 }
