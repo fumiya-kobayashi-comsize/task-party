42b7c7dd000e49c2eedbb2153d4445d47848b108
Negami27 <yohei.negami@comsize.com>
2023-08-07 15:07:58 +0900

    add:コメントデリートservlet,jsp,daoの追加

--- src/model/dao/CommentDeleteDAO.java
+++ src/model/dao/CommentDeleteDAO.java
@@ -1,5 +1,31 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * @author 根上
+ *
+ */
 public class CommentDeleteDAO {
 
+	/**
+	 * コメント 削除メソッド
+	 * @author 根上
+	 * @param commentId
+	 * @returnコメントを削除できたかを確認する変数count
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public int deleteComment(int commentId) throws ClassNotFoundException, SQLException {
+		int count = 0;
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement("DELETE FROM t_comment WHERE comment_id = ?")){
+			pstmt.setInt(1, commentId);
+			count = pstmt.executeUpdate();
+		}
+		return count;
+
+	}
 }
--- src/servlet/CommentDeleteServlet.java
+++ src/servlet/CommentDeleteServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.dao.CommentDeleteDAO;
 
 /**
  * Servlet implementation class CommentDeleteServlet
@@ -13,19 +18,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CommentDeleteServlet")
 public class CommentDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CommentDeleteServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CommentDeleteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -33,9 +39,26 @@ public class CommentDeleteServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		int count = 0;
+		CommentDeleteDAO deleteDAO = new CommentDeleteDAO();
+		int commentId = Integer.parseInt(request.getParameter("comment_id"));
+
+		try {
+			count = deleteDAO.deleteComment(commentId);
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		if (count == 1) {
+			RequestDispatcher rd = request.getRequestDispatcher("delete-comment-success.jsp");
+			rd.forward(request, response);
+		} else {
+			RequestDispatcher rd = request.getRequestDispatcher("delete-comment-error.jsp");
+			rd.forward(request, response);
+		}
 	}
 
 }
