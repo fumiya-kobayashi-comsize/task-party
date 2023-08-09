f9fe8f8db5b1c9ff8b7e308379bec035d9377aa0
Negami27 <yohei.negami@comsize.com>
2023-08-09 13:32:13 +0900

    add:タスクと紐づけられているコメントを全て削除するメソッドのテストクラス

---
+++ test/model/dao/CommentDeleteAllDAOTest.java
@@ -0,0 +1,40 @@
+package model.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * タスクに紐づいているコメントを全て削除するメソッドのテストクラス
+ * @author 根上
+ *
+ */
+public class CommentDeleteAllDAOTest {
+	@Test
+	void deleteAllComment() {
+		CommentDeleteAllDAO deleteAllDAO = new CommentDeleteAllDAO();
+		int count = 0;
+
+		try {
+//			comment tableにあるtaskIdを引数に入れる
+			count = deleteAllDAO.deleteAllComment(15);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		assertEquals(1, count);
+	}
+	@Test
+	void deleteAllCommentFailure() {
+		CommentDeleteAllDAO deleteAllDAO = new CommentDeleteAllDAO();
+		int count = 0;
+
+		try {
+			count = deleteAllDAO.deleteAllComment(0);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		assertEquals(0, count);
+	}
+}
