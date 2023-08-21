5673e2312b8060cb19464174fabac8dd7154db8e
KazusaKoseki <kazusa.koseki@comsize.com>
2023-08-21 17:58:11 +0900

    add:テストデータ用のJunitファイルとcsvを追加

---
+++ test/model/dao/TaskSelectCurrentUserDAOTest.java
@@ -0,0 +1,39 @@
+package model.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+class TaskSelectCurrentUserDAOTest {
+
+	@Test
+	void test1() {
+		int result = 0;
+		TaskSelectCurrentUserDAO dao = new TaskSelectCurrentUserDAO();
+
+		try {
+			result = dao.selectCurrentUsersTask("admin");
+
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		assertEquals(0, result);
+	}
+
+	@Test
+	void test2() {
+		int result = 0;
+		TaskSelectCurrentUserDAO dao = new TaskSelectCurrentUserDAO();
+
+		try {
+			result = dao.selectCurrentUsersTask("user");
+
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		assertEquals(9, result);
+	}
+
+}
